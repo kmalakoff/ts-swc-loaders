66bae4758bede0db4498d867cebaf563a1cb0341
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 12:26:46 -0800

    Update to ts-dev-stack

---
+++ src/index.esm.mjs
@@ -0,0 +1,4 @@
+export * from './esm/loaderCurrent.mjs';
+export * from './esm/loaderLegacy.mjs';
+
+export { default as spawnArgs } from './spawnArgs.cjs';
--- test/spec/commonjs.test.js
+++ test/spec/commonjs.test.js
@@ -1,13 +1,13 @@
-var assert = require('assert');
-var path = require('path');
-var spawn = require('cross-spawn-cb');
-var spawnArgs = require('../../lib/spawnArgs');
+const assert = require('assert');
+const path = require('path');
+const spawn = require('cross-spawn-cb');
+const spawnArgs = require('../..').spawnArgs;
 
-var DATA_DIR = path.resolve(__dirname, '..', 'data');
+const DATA_DIR = path.resolve(__dirname, '..', 'data');
 
 describe('commonjs', function () {
   it('tests', function (done) {
-    var args = spawnArgs('commonjs', 'mocha-compat', ['--watch-extensions', 'ts,tsx', 'test/commonjs/**/*.*'], { cwd: DATA_DIR, stdio: 'inherit' });
+    const args = spawnArgs('commonjs', 'mocha-compat', ['--watch-extensions', 'ts,tsx', 'test/commonjs/**/*.*'], { cwd: DATA_DIR, stdio: 'inherit' });
 
     spawn(args[0], args[1], args[2], function (err) {
       assert.ok(!err);

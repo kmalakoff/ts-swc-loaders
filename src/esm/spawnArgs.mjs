4804f75477a1b946ef3cad29bb204ea4becff25a
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-17 00:33:53 -0800

    Update spawnOptions for more flexible syntax

---
+++ src/esm/spawnArgs.mjs
@@ -0,0 +1,8 @@
+import process from 'process';
+import createSpawnArgs from '../createSpawnArgs.mjs';
+
+const local = +process.versions.node.split('.')[0];
+
+export default function spawnArgs(type, options, major) {
+  return createSpawnArgs(type, options, major || local);
+}

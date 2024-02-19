548a0cbf1cd614409d1b5a3ce461a48ea6adeee7
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-18 21:24:24 -0800

    Fix worker extension

--- dist/cjs/transformSync.js
+++ dist/cjs/transformSync.js
@@ -2,7 +2,7 @@
 var path = require("path");
 var major = +process.versions.node.split(".")[0];
 var version = major >= 14 ? "local" : "lts";
-var worker = path.resolve(__dirname, "workers", "transformSync.js");
+var worker = path.resolve(__dirname, "workers", "transformSync".concat(path.extname(__filename)));
 var call = null; // break dependencies
 module.exports = function transformSync(contents, filename, config) {
     if (!call) call = require("node-version-call"); // break dependencies
--- dist/esm/transformSync.cjs
+++ dist/esm/transformSync.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 const major = +process.versions.node.split('.')[0];
 const version = major >= 14 ? 'local' : 'lts';
-const worker = path.resolve(__dirname, 'workers', 'transformSync.js');
+const worker = path.resolve(__dirname, 'workers', `transformSync${path.extname(__filename)}`);
 let call = null; // break dependencies
 module.exports = function transformSync(contents, filename, config) {
     if (!call) call = require('node-version-call'); // break dependencies

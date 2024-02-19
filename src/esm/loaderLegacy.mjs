e836f2303b78339a55ff95b789bd412f36789b3b
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-19 12:37:00 -0800

    Put back yargs patch

--- src/esm/loaderLegacy.mjs
+++ src/esm/loaderLegacy.mjs
@@ -32,10 +32,12 @@ async function _getFormat(url, context, defaultGetFormat) {
 
   // file
   if (url.startsWith('file://')) {
-    const ext = path.extname(url);
-    let format = EXT_TO_FORMAT[ext];
-    if (!format) format = ext.length ? packageType(url) : 'commonjs'; // no extension assume commonjs
-    return { format };
+    let format = EXT_TO_FORMAT[path.extname(url)];
+    if (!format) format = packageType(url);
+    if (url.endsWith('/node_modules/yargs/yargs')) format = 'commonjs'; // args bin is cjs in a module
+    return {
+      format,
+    };
   }
 
   // relative

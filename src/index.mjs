9be861b0f59aec7278ba0d9f98e863cf96fa88d7
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-18 18:05:50 -0800

    Delay dependency loading

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import { register } from './cjs/register.mjs';
 register();
 
-export { default as transformSync } from './cjs/transformSync.cjs';
+export { default as transformSync } from './transformSync.cjs';
 export { default as createMatcher } from './createMatcher.mjs';
 export { default as spawnArgs } from './cjs/spawnArgs.cjs';

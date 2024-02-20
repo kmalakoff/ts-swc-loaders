240666adbaf3e0eadce2923ea4d5cd78836fbad3
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-19 21:56:06 -0800

    Fix ts-swc command

--- test/unit/cli.test.js
+++ test/unit/cli.test.js
@@ -5,6 +5,7 @@ delete process.env.NODE_OPTIONS;
 const assert = require('assert');
 const path = require('path');
 const spawn = require('cross-spawn-cb');
+const cr = require('cr');
 
 const DATA_DIR = path.resolve(__dirname, '..', 'data');
 const CLI = path.resolve(__dirname, '..', '..', 'bin', 'cli.js');
@@ -13,13 +14,17 @@ const CLI = path.resolve(__dirname, '..', '..', 'bin', 'cli.js');
 const major = +process.versions.node.split('.')[0];
 const type = major < 12 ? 'commonjs' : 'module';
 
-describe('cli', function () { 
-  it('run with cli option', function (done)  {
-      // cli(['./' + type + '/loader', './index.ts', 'arg'], { encoding: 'utf8', cwd: DATA_DIR }, function (err, res) {
-      spawn(CLI, ['./' + type + '/loader', './index.ts', 'arg'], { encoding: 'utf8', cwd: DATA_DIR }, function (err, res) {
-        var stdout = err ? err.stdout : res.stdout;
-        assert.equal(stdout, 'success: arg\n');
-        done();
-      });
+describe('cli', () => {
+  it('run with cli option', (done) => {
+    // cli(['./' + type + '/loader', './index.ts', 'arg'], { encoding: 'utf8', cwd: DATA_DIR }, function (err, res) {
+    spawn(CLI, [`./${type}/loader`, './index.ts', 'arg'], { encoding: 'utf8', cwd: DATA_DIR }, (err, res) => {
+      assert.equal(
+        cr(err ? err.stdout : res.stdout)
+          .split('\n')
+          .slice(-2)[0],
+        'success: arg'
+      );
+      done();
     });
+  });
 });

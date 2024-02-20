c4023aeb71304dd39693c8a3d1202f29dfe2fd84
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-19 22:39:50 -0800

    Add which for commands

--- src/cli.cjs
+++ src/cli.cjs
@@ -3,6 +3,7 @@ const path = require('path');
 const spawn = require('cross-spawn-cb');
 const pathKey = require('env-path-key');
 const prepend = require('path-string-prepend');
+const once = require('call-once-fn');
 const spawnParams = require('./index.mjs').spawnParams;
 
 const major = +process.versions.node.split('.')[0];
@@ -16,11 +17,21 @@ module.exports = function cli(args, options) {
   env[PATH_KEY] = prepend(env[PATH_KEY] || '', path.resolve(process.cwd(), 'node_modules', '.bin'));
   const params = spawnParams(type, { stdio: 'inherit', cwd, env, ...(options || {}) });
 
-  spawn(args[0], params.args.concat(args.slice(1)), params.options, (err) => {
+  const callback = once((err) => {
     if (err) {
       console.log(err.message);
       return exit(err.code || -1);
     }
     exit(0);
   });
+
+  if (params.options.NODE_OPTIONS || params.args[0] === '--require') {
+    spawn(args[0], params.args.concat(args.slice(1)), params.options, callback);
+  } else {
+    require('which')(args[0], { path: env[PATH_KEY] })
+      .then((cmd) => {
+        spawn('node', params.args.concat([cmd]).concat(args.slice(1)), params.options, callback);
+      })
+      .catch(callback);
+  }
 };

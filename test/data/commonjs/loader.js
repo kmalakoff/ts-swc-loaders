039a9de74611dba62f4250cfe549f11709387776
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 10:46:15 -0800

    Fix

--- test/data/commonjs/loader.js
+++ test/data/commonjs/loader.js
@@ -1,24 +1,5 @@
-var getopts = require('getopts-compat');
+require('ts-swc-loaders');
 
-console.log(process.argv)
-
-var index = process.argv.indexOf(__filename);
-if (index>0) process.argv = process.argv.slice(index+1);
-index = process.argv.indexOf(__filename.substring(0, __filename.lastIndexOf('.')));
-if (index>0) process.argv = process.argv.slice(index+1);
-
-var options = getopts(process.argv, {
-  alias: { require: 'r' },
-  stopEarly: true,
-});
-
-if (options.require) require(options.require);
-
-process.argv = options._;
-require(process.argv.shift());
-
-// require('ts-swc-loaders');
-
-// var index = 0;
-// while (process.argv[index].indexOf('.test.') < 0) index++;
-// require(process.argv[index]);
+var index = 0;
+while (process.argv[index].indexOf('.test.') < 0) index++;
+require(process.argv[index]);

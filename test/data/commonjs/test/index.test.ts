00d2d6b46b5af26b59e374b85f45ee4cd8658e3c
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 11:01:42 -0800

    Dsaadssad

--- test/data/commonjs/test/index.test.ts
+++ test/data/commonjs/test/index.test.ts
@@ -3,17 +3,20 @@ require('core-js/actual/map');
 
 console.log(1, process.argv);
 
+try {
 var App = require('./lib/react.tsx');
 console.log(2);
 var string = require('./lib/string.cjs');
-
+} catch (err) {
+  console.log(err)
+}
 console.log(3);
 
 if (!App.default) throw new Error('App not loaded');
 console.log(4);
 if (string !== 'string') throw new Error('String not equal to string')
 
-console.log(3);
+console.log(5);
 
 console.log('ran successfully');
 

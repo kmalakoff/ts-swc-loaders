f69eded59d07773fc2467e983e835debc81bb7a3
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 08:41:37 -0800

    Test

--- test/data/commonjs/loader.js
+++ test/data/commonjs/loader.js
@@ -1,5 +1,22 @@
-require('ts-swc-loaders');
+var getopts = require('getopts-compat');
 
-var index = 0;
-while (process.argv[index].indexOf('.test.') < 0) index++;
-require(process.argv[index]);
+var index = process.argv.indexOf(__filename);
+if (index>0) process.argv = process.argv.slice(index+1);
+index = process.argv.indexOf(__filename.substring(0, __filename.lastIndexOf('.')));
+if (index>0) process.argv = process.argv.slice(index+1);
+
+var options = getopts(process.argv, {
+  alias: { require: 'r' },
+  stopEarly: true,
+});
+
+if (options.require) require(options.require);
+
+process.argv = options._;
+require(process.argv.shift());
+
+// require('ts-swc-loaders');
+
+// var index = 0;
+// while (process.argv[index].indexOf('.test.') < 0) index++;
+// require(process.argv[index]);

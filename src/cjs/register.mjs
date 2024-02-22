55e89141478194fd5fb6005387a24c5452a35e85
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 16:18:25 -0800

    Lock @core/swc to working version

--- src/cjs/register.mjs
+++ src/cjs/register.mjs
@@ -7,11 +7,7 @@ import createMatcher from '../createMatcher.mjs';
 import extensions from '../extensions.mjs';
 import loadTSConfig from '../loadTSConfig.mjs';
 import transformSync from '../transformSync.cjs';
-
-const INTERNAL_PATHS = [path.resolve(__dirname, '..'), path.resolve(__dirname, '..', '..', 'node_modules')];
-function isInternal(x) {
-  return INTERNAL_PATHS.some((y) => x.startsWith(y));
-}
+import isInternal from './isInternal.cjs';
 
 const cache = new Cache();
 const config = loadTSConfig(path.resolve(process.cwd(), 'tsconfig.json'));

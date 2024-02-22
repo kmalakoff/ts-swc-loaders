f0bd94866085381672afd57d1f50a547f495be64
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 21:18:40 -0800

    Default load for no extension

--- test/data/module/test/index.test.ts
+++ test/data/module/test/index.test.ts
@@ -1,11 +1,14 @@
 import exit from 'exit';
 
 // @ts-ignore
-import App from './lib/react.tsx';
+import App from './lib/App.tsx';
 import string from './lib/string.cjs';
+// @ts-ignore
+import guess from './lib/guess';
 
 if (!App) throw new Error('App not loaded');
-if (string !== 'string') throw new Error('String not equal to string')
+if (string !== 'string') throw new Error('string not equal to string')
+if (guess !== 'guess') throw new Error('guess not equal to guess')
 
 console.log('Success!');
 

3b998ad19711e71de9c2cf8103145d7bd0c5d77f
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-21 21:01:34 -0800

    Remove internals and use exclude

--- src/esm/isInternal.mjs
+++
@@ -1,6 +0,0 @@
-const DIST = new URL('../..', import.meta.url).href;
-export default function isInternal(test) {
-  if (test.startsWith(DIST)) return true;
-  if (test.indexOf('/node_modules') >= 0) return true;
-  return false;
-}
--- test/data/commonjs/loader.js
+++ test/data/commonjs/loader.js
@@ -1,5 +1,5 @@
 require('ts-swc-loaders');
 
 var index = 0;
-while (process.argv[index].indexOf('.test.') < 0) index++;
+while (process.argv[index].indexOf('.test.') < 0 && index < process.argv.length) index++;
 require(process.argv[index]);

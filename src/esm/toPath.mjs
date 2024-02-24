4dec9eaae74eec36e9792e5a5ba495cb6e399f3e
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 00:55:39 -0800

    Implemented better path resolution

--- src/esm/toPath.mjs
+++ src/esm/toPath.mjs
@@ -3,7 +3,6 @@ import path from 'node:path';
 import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import resolve from 'resolve';
-import packageUp from './packageUp.mjs';
 
 const moduleRegEx = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/;
 
@@ -18,19 +17,21 @@ export default function toPath(x, context) {
   if (moduleRegEx.test(x)) {
     // biome-ignore lint/complexity/useOptionalChain: <explanation>
     const parentPath = context && context.parentURL ? path.dirname(toPath(context.parentURL)) : process.cwd();
+    let pkg = null;
     const main = resolve.sync(x, {
       basedir: parentPath,
       extensions: ['.js', '.json', '.node', '.mjs'],
+      packageFilter(json, dir) {
+        pkg = { json, dir };
+        return json;
+      },
     });
-    const pkg = packageUp(main);
-    if (!pkg || !pkg.json.module) return main;
-
-    // try resolving as a module
-    const modulePath = path.resolve(path.dirname(pkg.path), pkg.json.module);
-    if (pkg.json.name === x) return modulePath; // the module
+    if (!pkg || !pkg.json.module) return main; // no modules, use main
+    if (pkg.json.name === x) return path.resolve(pkg.dir, pkg.json.module); // the module
 
     // a relative path. Only accept if it doesn't break the relative naming and it exists
-    const mainPath = path.resolve(path.dirname(pkg.path), pkg.json.main);
+    const modulePath = path.resolve(pkg.dir, pkg.json.module);
+    const mainPath = path.resolve(pkg.dir, pkg.json.main);
     const moduleResolved = path.resolve(modulePath, path.relative(mainPath, main));
     return moduleResolved.indexOf(x.replace(pkg.json.name, '')) < 0 || !fs.existsSync(moduleResolved) ? main : moduleResolved;
   }

d3202ff1083b27bbe6b36363510f2cb691c406ca
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-23 17:30:38 -0800

    Added filtering of files

--- src/esm/loaderLegacy.mjs
+++ src/esm/loaderLegacy.mjs
@@ -1,49 +1,49 @@
 import path from 'path';
+import isBuiltinModule from 'is-builtin-module';
 
 import Cache from '../Cache.mjs';
 import createMatcher from '../createMatcher.mjs';
 import extensions from '../extensions.mjs';
 import loadTSConfig from '../loadTSConfig.mjs';
 import transformSync from '../transformSync.cjs';
-import packageType from './packageType.mjs';
+import extToFormat from './extToFormat.mjs';
+import fileType from './fileType.mjs';
 import toPath from './toPath.mjs';
-
-const EXT_TO_FORMAT = {
-  '.json': 'json',
-  '.mjs': 'module',
-  '.mts': 'module',
-  '.cjs': 'commonjs',
-  '.cts': 'commonjs',
-};
 
 const cache = new Cache();
 const config = loadTSConfig(path.resolve(process.cwd(), 'tsconfig.json'));
 const match = createMatcher(config);
 
+const typeFileRegEx = /^[^.]+\.d\.(.*)$/;
+
 async function _getFormat(url, context, next) {
+  if (isBuiltinModule(url)) return next(url, context);
   if (!url.startsWith('file://')) return await next(url, context);
   const filePath = toPath(url, context);
+  const ext = path.extname(filePath);
 
   // filtered
   if (!match(filePath)) {
-    if(!path.extname(filePath)) return { format: 'commonjs' }; // args bin is cjs in a module
+    if (!ext) return { format: 'commonjs' }; // args bin is cjs in a module
     return await next(url, context);
   }
 
   // file
-  const data = { format: EXT_TO_FORMAT[path.extname(url)] };
-  if (!data.format) data.format = await packageType(filePath);
+  const data = { format: extToFormat(ext) };
+  if (!data.format || ['.js', '.jsx'].indexOf(ext) >= 0) data.format = fileType(filePath);
   return data;
 }
 
 async function _transformSource(source, context, next) {
+  if (isBuiltinModule(context.url)) return next(source, context);
   const loaded = await next(source, context);
   const filePath = toPath(context.url);
+  const ext = path.extname(filePath);
 
   // filtered
   if (!match(filePath)) return loaded;
-  if (filePath.endsWith('.d.ts')) return { source: '' };
-  if (extensions.indexOf(path.extname(filePath)) < 0) return loaded;
+  if (typeFileRegEx.test(filePath)) return { source: '' };
+  if (extensions.indexOf(ext) < 0) return loaded;
 
   const contents = loaded.source.toString();
   const data = cache.getOrUpdate(cache.cachePath(filePath, config), contents, () => transformSync(contents, filePath, config));

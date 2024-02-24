7704d9cc40ebab5760b433aa0eeea861fb5c2c22
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-23 17:30:38 -0800

    Added filtering of files

--- src/esm/loaderCurrent.mjs
+++ src/esm/loaderCurrent.mjs
@@ -1,85 +1,95 @@
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
+import { pathToFileURL } from 'node:url';
+import isBuiltinModule from 'is-builtin-module';
 
 import Cache from '../Cache.mjs';
 import createMatcher from '../createMatcher.mjs';
 import extensions from '../extensions.mjs';
 import loadTSConfig from '../loadTSConfig.mjs';
 import transformSync from '../transformSync.cjs';
-import packageType from './packageType.mjs';
+import extToFormat from './extToFormat.mjs';
+import fileType from './fileType.mjs';
 import toPath from './toPath.mjs';
 
 const major = +process.versions.node.split('.')[0];
 const importJSONKey = major >= 18 ? 'importAttributes' : 'importAssertions';
 
-const moduleRegEx = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/;
-const indexExtensions = extensions.map((x) => `index${x}`);
-
 const cache = new Cache();
 const config = loadTSConfig(path.resolve(process.cwd(), 'tsconfig.json'));
 const match = createMatcher(config);
 
+const moduleRegEx = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/;
+const typeFileRegEx = /^[^.]+\.d\.(.*)$/;
+const indexExtensions = extensions.map((x) => `index${x}`);
+
 export async function resolve(specifier, context, next) {
-  if (specifier.startsWith('node:')) return next(specifier, context); // TODO: optimize, but isBuiltin not available on older node
+  if (isBuiltinModule(specifier)) return next(specifier, context);
   const filePath = toPath(specifier, context);
+  const ext = path.extname(filePath);
+  let stats;
+  try {
+    stats = await fs.stat(filePath);
+  } catch (_err) {}
 
   // filtered
   if (!match(filePath)) {
     const data = await next(specifier, context);
-    if (!data.format) data.format = 'commonjs'
-    if(path.isAbsolute(filePath) && !path.extname(filePath)) data.format = 'commonjs'; // args bin is cjs in a module
+    if (!data.format) data.format = 'commonjs';
+    if (path.isAbsolute(filePath) && !ext) data.format = 'commonjs'; // args bin is cjs in a module
     return data;
   }
 
   // directory
-  if (specifier.endsWith('/')) {
+  // biome-ignore lint/complexity/useOptionalChain: <explanation>
+  if (specifier.endsWith('/') || (stats && stats.isDirectory())) {
     const items = await fs.readdir(filePath);
-    for (const item of items) {
-      if (indexExtensions.indexOf(item) >= 0) {
-        return await resolve(specifier + item, context, next);
-      }
-    }
+    const item = items.find((x) => indexExtensions.indexOf(x) >= 0);
+    if (item) return await resolve(`${specifier}${specifier.endsWith('/') ? '' : '/'}${item}`, context, next);
   }
   // look up the extension
-  else if (!path.extname(specifier) && !moduleRegEx.test(specifier)) {
-    const fileName = path.basename(filePath);
+  else if ((!ext && !moduleRegEx.test(specifier)) || !stats) {
+    const fileName = path.basename(filePath).replace(/(\.[^/.]+)+$/, '');
     const items = await fs.readdir(path.dirname(filePath));
-    const found = items.find((x) => x.startsWith(fileName) && extensions.indexOf(path.extname(x)) >= 0);
+    const found = items.find((x) => x.startsWith(fileName) && !typeFileRegEx.test(x) && extensions.indexOf(path.extname(x)) >= 0);
     if (found) return await resolve(specifier + path.extname(found), context, next);
   }
 
   // use default resolve and infer from package type
-  const data = await next(specifier, context);
-  if (!data.format) data.format = packageType(filePath);
+  const data = {
+    url: pathToFileURL(filePath).href,
+    format: extToFormat(ext),
+    shortCircuit: true,
+  };
+  if (!data.format) data.format = fileType(filePath);
   return data;
 }
 
 export async function load(url, context, next) {
-  if (url.startsWith('node:')) return await next(url, context, next);
+  if (isBuiltinModule(url)) return next(url, context);
   if (url.endsWith('.json')) context[importJSONKey] = Object.assign(context[importJSONKey] || {}, { type: 'json' });
 
   const loaded = await next(url, context);
-  const filePath = toPath(url, context);
-  const hasSource = loaded.source;
-  if (!hasSource) loaded.source = await fs.readFile(filePath);
+  const filePath = toPath(loaded.responseURL || url, context);
+  const ext = path.extname(filePath);
 
   // filtered
   if (!match(filePath)) return loaded;
-  if (url.endsWith('.d.ts'))
+  if (typeFileRegEx.test(filePath))
     return {
       ...loaded,
       format: 'module',
       source: '',
     };
-  if (extensions.indexOf(path.extname(filePath)) < 0) return loaded;
+  if (extensions.indexOf(ext) < 0) return loaded;
 
   // transform
+  if (!loaded.source) loaded.source = await fs.readFile(filePath);
   const contents = loaded.source.toString();
   const data = cache.getOrUpdate(cache.cachePath(filePath, config), contents, () => transformSync(contents, filePath, config));
   return {
     ...loaded,
-    format: hasSource ? 'module' : 'commonjs',
     source: data.code,
     shortCircuit: true,
   };

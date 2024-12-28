d20b26acd84cb951cf7ee739d36af0c10c96d6b6
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 00:22:02 -0800

    Upgrade modules

--- src/parse.ts
+++ src/parse.ts
@@ -3,13 +3,13 @@ import url from 'url';
 import spawn from 'cross-spawn-cb';
 import packageRoot from './lib/packageRoot.js';
 
-const major = +process.versions.node.split('.')[0];
+const _major = +process.versions.node.split('.')[0];
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const root = packageRoot(__dirname);
 
 const loaderCJS = path.resolve(root, 'dist', 'cjs', 'index.cjs.js');
-const loaderESM = !url.pathToFileURL || url.pathToFileURL(path.resolve(root, 'dist', 'esm', 'index.esm.mjs'));
-// const importArgs = `data:text/javascript,import { register } from "node:module"; register("ts-swc-loaders", "${loaderESM}");`;
+const loaderESM = path.resolve(root, 'dist', 'esm', 'index.esm.mjs');
+const _importArgs = `data:text/javascript,import { register } from "node:module"; register("ts-swc-loaders", "file:////${loaderESM}");`;
 
 import type { ParseResult, SpawnOptions } from './types.js';
 
@@ -22,12 +22,15 @@ export default function parse(type: string, command: string, args: string[], opt
     return parsed;
   }
 
-  // https://nodejs.org/api/module.html#moduleregisterspecifier-parenturl-options
-  // v20.8.0
-  // if (major >= 18) return { args: ['--no-warnings=ExperimentalWarning', '--import', importArgs], options };
+  // if (major > 4) parsed.args.unshift('--no-warnings=ExperimentalWarning');
 
-  // if (major <= 16) parsed.args.unshift('--experimental-modules');
-  if (major > 4) parsed.args.unshift('--no-warnings=ExperimentalWarning');
+  // https://nodejs.org/api/module.html#moduleregisterspecifier-parenturl-options
+  // if (major >= 18) {
+  //   parsed.args = ['--import', importArgs].concat(parsed.args);
+  //   console.log(parsed)
+  //   return parsed;
+  // }
+
   parsed.options.env.NODE_OPTIONS = `--loader ${loaderESM}${options.env.NODE_OPTIONS ? ` ${options.env.NODE_OPTIONS}` : ''}`;
   return parsed;
 }

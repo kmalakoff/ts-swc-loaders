a86de2868eb713bd959b63cdbec267dbdd661f03
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-27 20:17:31 -0800

    refactor: rename spawnParams to parse and improve argument handling

--- test/data/module/loader.js
+++ test/data/module/loader.js
@@ -1,7 +1,3 @@
-import process from 'node:process';
+#!/usr/bin/env node
 
-import 'ts-swc-loaders';
-
-var index = 0;
-while (process.argv[index].indexOf('.test.') < 0 && index < process.argv.length) index++;
-import(process.argv[index]);
+import './shim.js'
--- test/unit/commonjs.test.cjs
+++ test/unit/commonjs.test.cjs
@@ -4,9 +4,10 @@ delete process.env.NODE_OPTIONS;
 const assert = require('assert');
 const path = require('path');
 const spawn = require('cross-spawn-cb');
-const { spawnParams } = require('ts-swc-loaders');
 const { linkModule, unlinkModule } = require('module-link-unlink');
 const cr = require('cr');
+
+const { parse } = require('ts-swc-loaders');
 
 const major = +process.versions.node.split('.')[0];
 const type = 'commonjs';
@@ -14,12 +15,11 @@ const MODULE_DIR = path.resolve(__dirname, '..', '..');
 const DATA_DIR = path.resolve(__dirname, '..', 'data', type);
 const DATA_MODULE_DIR = path.join(DATA_DIR, 'node_modules');
 
-const args = spawnParams(type, { cwd: DATA_DIR, encoding: 'utf8' });
-
 describe('commonjs', () => {
   major > 0 ||
     it('loader', (done) => {
-      spawn('./loader', args.args.concat(['./test/index.test.ts', 'arg']), args.options, (err, res) => {
+      const parsed = parse(type, './loader.js', ['./test/index.test.ts'], { cwd: DATA_DIR, encoding: 'utf8' });
+      spawn(parsed.command, parsed.args, parsed.options, (err, res) => {
         assert.ok(!err, err ? err.message : '');
         assert.equal(cr(res.stdout).split('\n').slice(-2)[0], 'Success!');
         done();
@@ -28,7 +28,8 @@ describe('commonjs', () => {
 
   major <= 0 ||
     it('node', (done) => {
-      spawn(process.execPath, args.args.concat(['./test/index.test.ts', 'arg']), args.options, (err, res) => {
+      const parsed = parse(type, process.execPath, ['./test/index.test.ts'], { cwd: DATA_DIR, encoding: 'utf8' });
+      spawn(parsed.command, parsed.args, parsed.options, (err, res) => {
         assert.ok(!err, err ? err.message : '');
         assert.equal(cr(res.stdout).split('\n').slice(-2)[0], 'Success!');
         done();

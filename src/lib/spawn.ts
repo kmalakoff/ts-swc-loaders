20560cc06232ee750c9e2ce00fa635016c18e16e
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-05 08:04:47 -0800

    suppress experimental loader warnings during spawn

--- src/lib/spawn.ts
+++ src/lib/spawn.ts
@@ -1,10 +1,19 @@
-import spawn from 'cross-spawn-cb';
+import spawn, { type SpawnResult } from 'cross-spawn-cb';
 import resolveBin from 'resolve-bin-sync';
 import { moduleRegEx } from '../constants';
 import parse from './parse';
 
 const major = +process.versions.node.split('.')[0];
 const type = major < 12 ? 'commonjs' : 'module';
+
+// https://github.com/yarnpkg/berry/blob/2cf0a8fe3e4d4bd7d4d344245d24a85a45d4c5c9/packages/yarnpkg-pnp/sources/loader/applyPatch.ts#L414-L435
+const originalEmit = process.emit;
+// @ts-expect-error - TS complains about the return type of originalEmit.apply
+process.emit = (name, data, ..._args) => {
+  if (name === 'warning' && typeof data === 'object' && data.name === 'ExperimentalWarning' && (data.message.includes('--experimental-loader') || data.message.includes('Custom ESM Loaders is an experimental feature'))) return false;
+
+  return originalEmit.call(process, name, data, ..._args);
+};
 
 function which(command) {
   try {

b1b107d5e7e3b55d60f031f852f2c7a0898745d9
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-05 18:03:59 -0800

    Upgrade modules

--- src/lib/spawn.ts
+++ src/lib/spawn.ts
@@ -2,6 +2,7 @@ import spawn from 'cross-spawn-cb';
 import resolveBin from 'resolve-bin-sync';
 import { moduleRegEx } from '../constants';
 import parse from './parse';
+import suppressWarnings from './suppressWarnings';
 
 const major = +process.versions.node.split('.')[0];
 const type = major < 12 ? 'commonjs' : 'module';
@@ -18,5 +19,9 @@ export default function worker(command, args, options, callback) {
   const env = options.env || process.env;
 
   const parsed = parse(type, which(command), args, { stdio: 'inherit', cwd, env, ...options });
-  spawn(parsed.command, parsed.args, parsed.options, callback);
+  const restore = suppressWarnings();
+  spawn(parsed.command, parsed.args, parsed.options, (err, res) => {
+    restore();
+    err ? callback(err) : callback(err, res);
+  });
 }

fe3969508144cf0faeb60ad85133efe43f1acfa2
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-16 07:18:35 -0800

    Upgrade modules

--- src/index-base.ts
+++ src/index-base.ts
@@ -1,4 +1,6 @@
 export type * from './types';
 export { default as parse } from './lib/parse';
 export { default as spawn } from './lib/spawn';
+
 export { default as cache } from './cache';
+export type { default as Cache } from './lib/Cache';

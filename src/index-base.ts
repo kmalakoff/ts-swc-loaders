d2f752884dbf164016fc1a896c75519a6ed703a0
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-15 18:38:41 -0800

    Upgrade modules

--- src/index-base.ts
+++ src/index-base.ts
@@ -1,4 +1,4 @@
 export type * from './types';
 export { default as parse } from './lib/parse';
-export { default as clean } from './lib/clean';
 export { default as spawn } from './lib/spawn';
+export { default as cache } from './cache';
--- src/lib/clean.ts
+++
@@ -1,9 +0,0 @@
-import rimraf2 from 'rimraf2';
-import { DEFAULT_CACHE_PATH } from '../constants';
-
-import type { CleanOptions } from '../types';
-
-export default function clean(_options?: CleanOptions) {
-  rimraf2.sync(DEFAULT_CACHE_PATH, { disableGlob: true });
-  console.log(`Cleaned ${DEFAULT_CACHE_PATH}`);
-}

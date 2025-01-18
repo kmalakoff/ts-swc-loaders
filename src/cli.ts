d041a34868c68e55126775e4224c0ce4b75cf718
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-17 20:36:02 -0800

    1.12.41

--- src/cli.ts
+++ src/cli.ts
@@ -19,21 +19,9 @@ export default function cli(argv) {
     return exit(options.clear ? 0 : ERROR_CODE);
   }
 
-  const next = (err, _res) => {
-    if (err && err.status === 3221226505) err = null; // windows can give spurious errors
-    exit(err ? ERROR_CODE : 0);
-  };
-
-  // DEBUG MODE
-  if (typeof process.env.DEBUG !== 'undefined') {
-    options.encoding = 'utf8';
-    return run(args[0], args.slice(1), options, (err, res) => {
-      if (err) console.log(JSON.stringify({ err, res }));
-      console.log((res || err || {}).stdout);
-      next(err, res);
-    });
-  }
-
   options.stdio = 'inherit'; // pass through stdio
-  return run(args[0], args.slice(1), options, next);
+  return run(args[0], args.slice(1), options, (err) => {
+    if (err && err.status === 3221226505) err = null; // windows can give spurious errors on node 18
+    exit(err ? ERROR_CODE : 0);
+  });
 }

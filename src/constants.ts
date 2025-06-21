b53c0aa309aa87609985f6cea0a43e7d1ea3a580
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-20 21:29:34 -0700

    Comprehensive types

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import homedir from 'homedir-polyfill';
 import path from 'path';
 
-export const DEFAULT_CACHE_PATH = path.join(homedir(), '.ts-swc');
+export const DEFAULT_CACHE_PATH: string = path.join(homedir(), '.ts-swc');
 export const moduleRegEx = /^[^./]|^\.[^./]|^\.\.[^/]/;
 export const typeFileRegEx = /^[^.]+\.d\.[cm]?ts$/;

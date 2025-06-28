44b2f576b06f509e2e0a0acc3d890fc2b9dbc7da
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-28 16:01:40 -0700

    Upgrade modules

--- src/esm/fileType.ts
+++ src/esm/fileType.ts
@@ -1,24 +1,36 @@
+import fs from 'fs';
+import moduleRoot from 'module-root-sync';
 import path from 'path';
 import startsWith from 'starts-with';
-import type { PackageInfo } from '../types.ts';
-import packageUp from './packageUp.ts';
 
-function isEntry(filePath: string, pkg: PackageInfo, key: string) {
-  if (pkg.json[key] === undefined) return false;
-  const modulePath = path.join(pkg.dir, pkg.json[key]);
+function isEntry(contents: Record<string, string>, key: string, packageDir: string, filePath: string) {
+  if (contents[key] === undefined) return false;
+  const modulePath = path.join(packageDir, contents[key] as string);
   if (filePath === modulePath) return true;
   const moduleDir = path.dirname(modulePath);
   if (startsWith(filePath, moduleDir)) return true;
   return false;
 }
 
-export default function fileType(filePath: string) {
-  const pkg = packageUp(filePath);
-  if (!pkg) return 'commonjs';
-  if (isEntry(filePath, pkg, 'module')) return 'module';
-  if (isEntry(filePath, pkg, 'main')) return 'commonjs';
-  if (pkg.json.type) return pkg.json.type;
-  if (pkg.json.module) return 'module';
-  if (pkg.json.main) return 'commonjs';
+function classifyPackage(packageDir: string, filePath: string): string {
+  const contents = JSON.parse(fs.readFileSync(path.join(packageDir, 'package.json'), 'utf8'));
+  if (isEntry(contents, 'module', packageDir, filePath)) return 'module';
+  if (isEntry(contents, 'main', packageDir, filePath)) return 'commonjs';
+  if (contents.type) return contents.type as string;
+  if (contents.module) return 'module';
+  if (contents.main) return 'commonjs';
   return 'commonjs';
+}
+
+const cache: Record<string, string> = {};
+
+export default function fileType(filePath: string): string {
+  const fileDir = path.dirname(filePath);
+  if (cache[fileDir] !== undefined) return cache[fileDir]; // already cached
+
+  const packageDir = moduleRoot(fileDir);
+  const type = packageDir ? classifyPackage(packageDir, filePath) : 'commonjs';
+  cache[fileDir] = type;
+  cache[packageDir] = type;
+  return type;
 }

fa433b8590f18137fb189fac743cd48605eaef97
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-22 13:46:02 -0700

    Upgrade modules

--- bin/cli.js
+++ bin/cli.js
@@ -2,5 +2,5 @@
 
 if (typeof require === 'undefined')
   // biome-ignore lint/security/noGlobalEval: dual esm and cjs
-  eval("import('../dist/esm/cli.js').then((cli) => cli.default(process.argv.slice(2), 'ts-swc')).catch((err) => { console.log(err); process.exit(-1); });");
+  eval("import('../dist/esm/cli.js').then((cli) => cli.default(process.argv.slice(2), 'ts-swc')).catch((err) => { console.error(err); process.exit(-1); });");
 else require('../dist/cjs/cli.js')(process.argv.slice(2), 'ts-swc');
